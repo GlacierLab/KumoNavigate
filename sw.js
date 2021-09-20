60f996a3fe994a2d32dfd87e3ee88b32f0ad7ce8
琴梨梨 <yeyaohua2010@163.com>
2021-09-20 08:39:22 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,17 +10,19 @@ var URLS = [
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                fetch(e.request).then(function (response) {
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            }
+        caches.open(CACHE_NAME).then(function (cache) {
+            caches.match(e.request).then(function (request) {
+                if (request) { // if cache is available, respond with cache
+                    console.log('responding with cache : ' + e.request.url)
+                    return request
+                } else {       // if there are no cache, try fetching request
+                    console.log('file is not cached, fetching : ' + e.request.url)
+                    fetch(e.request).then(function (response) {
+                        cache.put(e.request, response.clone());
+                        return response;
+                    });
+                }
+            })
         })
     )
 })
