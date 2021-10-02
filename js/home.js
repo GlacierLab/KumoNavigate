a00d0e62c89b4d97134a5dc46cb57c91113e4fb2
琴梨梨 <yeyaohua2010@163.com>
2021-10-02 16:43:35 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,31 @@
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js', {scope: '/'})
   }
+function getVideoCardInfo() {
+    const gl = document.createElement('canvas').getContext('webgl');
+    if (!gl) {
+        return false;
+    }
+    const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+    return debugInfo ? {
+        vendor: gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL),
+        renderer: gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL),
+    } : false;
+}
+var GPUinfo = getVideoCardInfo()
+if (GPUinfo) {
+    var GPUname = GPUinfo.renderer;
+    var GPUdriver = false;
+    if (GPUinfo.renderer.indexOf("ANGLE") == 0) {
+        GPUdriver = GPUname.substring(GPUname.indexOf("-") + 1, GPUname.indexOf(")"))
+        GPUname = GPUname.substring(GPUname.indexOf(",") + 2, GPUname.indexOf("Direct3D") - 1);
+    }
+    var GPUele = document.createElement("h6")
+    GPUele.innerHTML += "显卡型号：" + atob("PGNvZGUgY2xhc3M9Imxhbmd1YWdlLXBsYWludGV4dCBoaWdobGlnaHRlci1yb3VnZSI+") + GPUname + "</code>"
+    if (GPUdriver) {
+        GPUele.innerHTML += "显卡驱动：" + atob("PGNvZGUgY2xhc3M9Imxhbmd1YWdlLXBsYWludGV4dCBoaWdobGlnaHRlci1yb3VnZSI+") + GPUdriver + "</code>"
+    }
+    document.getElementsByTagName("hr")[1].parentNode.insertBefore(GPUele, document.getElementsByTagName("hr")[1])
+} else {
+    console.log("Cannot Get GPU Info!")
+}
