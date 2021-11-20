c91ab725f3cc8dd66e1cd6f02ca539f69a2485da
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2021-11-20 13:32:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = '云酱游戏导航'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = '20211002'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = '20211120'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [
     '/',
