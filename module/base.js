1a0604ddf18e9652580d3fa94910098c710404d5
琴梨梨OvO <24567775+qinlili23333@users.noreply.github.com>
2024-11-17 12:13:03 +1000

    合并ctfileGet的基础模块更新

--- module/base.js
+++ module/base.js
@@ -28,16 +28,61 @@ const util = {
     }
 }
 let ModuleCache = {};
-const ModuleLoader = {
+window.ModuleLoader = {
+    useRedbankLoaderGlobal: true,
+    pendingCalls: [],
+    loadedRemote: [],
+    checkPending: async () => {
+        for (const call of ModuleLoader.pendingCalls) {
+            if (Module[call.name]) {
+                await call.callback();
+            }
+        }
+    },
     loadLocal: async (part, initList) => {
         Module = Object.assign(Module, part);
         await Module.autoinit.initList(initList ? initList : Object.keys(part));
+        await ModuleLoader.checkPending();
     },
     loadRemote: async (name, initList) => {
         //加载远程模块
         await util.loadScriptAsync("module/" + name + ".js");
         await ModuleLoader.loadLocal(ModuleCache[name], initList);
         delete ModuleCache[name];
+        ModuleLoader.loadedRemote.push(name);
+    },
+    queueLoaded: async (name, callback) => {
+        //确定已经置入队列加载的模块
+        //调用前请确保已调用模块加载器
+        if (Module[name]) {
+            return await callback();
+        } else {
+            ModuleLoader.pendingCalls.append({ name: name, callback: callback });
+            //-10表示模块尚未就绪，就绪后会自动调用
+            //模块就绪时间不定，可能需要覆盖和取消的操作不要用这个
+            return -10;
+        }
+
+    },
+    ensureLoaded: async (name, modulename, callback) => {
+        //不确定是否调用模块加载器的情况
+        //确保模块加载完成执行后再回调
+        if (Module[name]) {
+            return await callback();
+        } else {
+            //高优先级加载
+            if (ModuleLoader.loadedRemote.includes(modulename)) {
+                //已经加载过指定模块，但模块内没有这个组件
+                return -1;
+            } else {
+                await ModuleLoader.loadRemote(modulename);
+                if (Module[name]) {
+                    return await callback();
+                }
+                //加载了，但并没有这个组件
+                return -1;
+            }
+        }
     }
 }
 let Module = {
@@ -143,7 +188,7 @@ let Module = {
     },
     yuni: {
         lazyinit: () => {
-            util.loadCssCode(`#setFrame {
+            util.loadCssCode(`#yuniFrame {
 max-width: 600px;
 margin: auto;
 padding: 100%;
@@ -214,18 +259,18 @@ right: 0px;
             const framediv = `<div id="yuniContainer">
 <div id="yuni"> 
 <img role="button" aria-label="关闭弹出页面" id="closeBtn" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48c3ZnIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDQ4IDQ4IiBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGQ9Ik02IDlMNDIgOSIgc3Ryb2tlPSIjMzMzIiBzdHJva2Utd2lkdGg9IjMiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPjxwYXRoIGQ9Ik02IDE5TDQyIDE5IiBzdHJva2U9IiMzMzMiIHN0cm9rZS13aWR0aD0iMyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+PHBhdGggZD0iTTYgMjZMMjQgNDBMNDIgMjYiIHN0cm9rZT0iIzMzMyIgc3Ryb2tlLXdpZHRoPSIzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz48L3N2Zz4=">
-<iframe title="弹出页面" id="setFrame" frameborder="0"></iframe>
+<iframe title="弹出页面" id="yuniFrame" frameborder="0"></iframe>
 <img alt="加载中" id="loading" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjxzdmcgd2lkdGg9JzEyMHB4JyBoZWlnaHQ9JzEyMHB4JyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAgMTAwIiBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJ4TWlkWU1pZCI+DQogICAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEwMCIgaGVpZ2h0PSIxMDAiIGZpbGw9Im5vbmUiIGNsYXNzPSJiayI+PC9yZWN0Pg0KICAgIDxjaXJjbGUgY3g9IjUwIiBjeT0iNTAiIHI9IjQwIiBzdHJva2U9IiNEQ0FDODkiIGZpbGw9Im5vbmUiIHN0cm9rZS13aWR0aD0iMTAiIHN0cm9rZS1saW5lY2FwPSJidXR0Ij4NCiAgICAgICAgPGFuaW1hdGUgYXR0cmlidXRlTmFtZT0ic3Ryb2tlLWRhc2hvZmZzZXQiIGR1cj0iMnMiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiBmcm9tPSIwIiB0bz0iNTAyIj48L2FuaW1hdGU+DQogICAgICAgIDxhbmltYXRlIGF0dHJpYnV0ZU5hbWU9InN0cm9rZS1kYXNoYXJyYXkiIGR1cj0iMnMiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiB2YWx1ZXM9IjE1MC42IDEwMC40OzEgMjUwOzE1MC42IDEwMC40Ij48L2FuaW1hdGU+DQogICAgPC9jaXJjbGU+DQo8L3N2Zz4=">
 </div>
 </div>`
             document.body.insertAdjacentHTML('beforeend', framediv);
             //弹出
             const rebuildFrame = url => {
-                if (document.getElementById("setFrame")) {
-                    document.getElementById("yuni").removeChild(document.getElementById("setFrame"));
+                if (document.getElementById("yuniFrame")) {
+                    document.getElementById("yuni").removeChild(document.getElementById("yuniFrame"));
                 };
                 let newFrame = document.createElement("iframe");
-                newFrame.id = "setFrame";
+                newFrame.id = "yuniFrame";
                 newFrame.frameBorder = 0;
                 document.getElementById("closeBtn").insertAdjacentElement("afterend", newFrame)
                 newFrame.src = url;
@@ -239,10 +284,7 @@ right: 0px;
                 console.log(event.data)
                 if (typeof event.data == "string") {
                     if (event.data == "close") {
-                        document.getElementById("yuniContainer").style.opacity = 0;
-                        document.getElementById("yuni").style.marginTop = "100vh";
-                        await util.sleep(500);
-                        document.getElementById("yuniContainer").style.display = "none";
+                        Module.yuni.close();
                     }
                     if (event.data.startsWith("http")) {
                         document.getElementById("loading").style.display = "block";
@@ -259,21 +301,49 @@ right: 0px;
                 document.getElementById("loading").style.display = "block";
                 rebuildFrame(url);
                 if (wide) {
-                    document.getElementById("setFrame").style.maxWidth = "100vw";
+                    document.getElementById("yuniFrame").style.maxWidth = "100vw";
                 }
-            }
-            document.getElementById("closeBtn").addEventListener("click", async (event) => {
-                event.stopPropagation();
+            };
+            Module.yuni.showNative = async (url, wide) => {
+                //使用iframe渲染设置
+                document.getElementById("yuniContainer").style.display = "block";
+                await util.sleep(1);
+                document.getElementById("yuniContainer").style.opacity = 1;
+                document.getElementById("yuni").style.marginTop = "10vh";
+                document.getElementById("loading").style.display = "block";
+                if (document.getElementById("yuniFrame")) {
+                    document.getElementById("yuni").removeChild(document.getElementById("yuniFrame"));
+                };
+                let newFrame = document.createElement("div");
+                newFrame.id = "yuniFrame";
+                newFrame.style.overflow = "auto";
+                document.getElementById("closeBtn").insertAdjacentElement("afterend", newFrame);
+                if (wide) {
+                    document.getElementById("yuniFrame").style.maxWidth = "100vw";
+                }
+                util.setInnerHTML(newFrame, await (await fetch("/yuni/" + url + ".yuniml")).text());
+                newFrame.style.padding = "0px";
+                document.getElementById("loading").style.display = "none";
+            };
+            Module.yuni.close = async () => {
                 document.getElementById("yuni").style.marginTop = "100vh";
                 document.getElementById("yuniContainer").style.opacity = 0;
                 await util.sleep(500);
                 document.getElementById("yuniContainer").style.display = "none";
-                document.getElementById("yuni").removeChild(document.getElementById("setFrame"));
+                document.getElementById("yuni").removeChild(document.getElementById("yuniFrame"));
+            };
+            document.getElementById("closeBtn").addEventListener("click", async (event) => {
+                event.stopPropagation();
+                Module.yuni.close();
             })
         },
         showFrame: async (url, wide) => {
             Module.yuni.lazyinit();
-            Module.yuni.showFrame(url, wide);
+            await Module.yuni.showFrame(url, wide);
+        },
+        showNative: async (url, wide) => {
+            Module.yuni.lazyinit();
+            await Module.yuni.showNative(url, wide);
         }
     }
 }
